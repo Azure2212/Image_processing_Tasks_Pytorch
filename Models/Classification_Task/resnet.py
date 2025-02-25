0ad1588b5aa56126427b2eafcb0b4c7e002bae65
Azure2212 <dasanbob22122002@gmail.com>
2025-02-25 21:37:53 +0700

    update

--- Models/Classification_Task/resnet.py
+++ Models/Classification_Task/resnet.py
@@ -33,8 +33,9 @@ model_urls = {
     "wide_resnet50_2": "https://download.pytorch.org/models/wide_resnet50_2-95faca4d.pth",
     "wide_resnet101_2": "https://download.pytorch.org/models/wide_resnet101_2-32ee1156.pth",
     #"vggface2": "https://onedrive.live.com/download?cid=D07B627FBE5CFFFA&resid=D07B627FBE5CFFFA%21587&authkey=APXT_JMvytW7cgk",
-    "vggface2": "/kaggle/input/resnet50-vggface2-weight/resnet50_scratch_weight.pkl",
+    ##"vggface2": "/kaggle/input/resnet50-vggface2-weight/resnet50_scratch_weight.pkl",
     "vggface2_ft": "/kaggle/input/resnet50-vggface2-weight/resnet50_ft_weight.pkl",
+    #"vggface2_ft": "/kaggle/input/resnet50-vggface2-freezed-except-cbam/ResnetDuck_Cbam_cuaTuan"
 }
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
@@ -55,12 +56,104 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-#### CBAM BLOCK
-import os
-import torch
-import torch.nn as nn
-from .resnet_cbam_pytorchcv.common import conv1x1_block, conv7x7_block, calc_net_weights
-from .resnet_cbam_pytorchcv.resnet import ResInitBlock, ResBlock, ResBottleneck
+class ConvBlock(nn.Module):
+    """
+    Standard convolution block with Batch normalization and activation.
+
+    Parameters
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    kernel_size : int or tuple(int, int)
+        Convolution window size.
+    stride : int or tuple(int, int)
+        Strides of the convolution.
+    padding : int or tuple(int, int) or tuple(int, int, int, int)
+        Padding value for convolution layer.
+    dilation : int or tuple(int, int), default 1
+        Dilation value for convolution layer.
+    groups : int, default 1
+        Number of groups.
+    bias : bool, default False
+        Whether the layer uses a bias vector.
+    use_bn : bool, default True
+        Whether to use BatchNorm layer.
+    bn_eps : float, default 1e-5
+        Small float added to variance in Batch norm.
+    activation : function or str or None, default nn.ReLU(inplace=True)
+        Activation function or name of activation function.
+    """
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: int | tuple[int, int],
+                 stride: int | tuple[int, int],
+                 padding: int | tuple[int, int] | tuple[int, int, int, int],
+                 dilation: int | tuple[int, int] = 1,
+                 groups: int = 1,
+                 bias: bool = False,
+                 use_bn: bool = True,
+                 bn_eps: float = 1e-5,
+                 activation = None):
+        super(ConvBlock, self).__init__()
+        self.activate = (activation is not None)
+        self.use_bn = use_bn
+        self.use_pad = (isinstance(padding, (list, tuple)) and (len(padding) == 4))
+
+        if self.use_pad:
+            self.pad = nn.ZeroPad2d(padding=padding)
+            padding = 0
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+        if self.use_bn:
+            self.bn = nn.BatchNorm2d(
+                num_features=out_channels,
+                eps=bn_eps)
+        if self.activate:
+            self.activ = get_activation_layer(activation)
+
+    def forward(self, x):
+        if self.use_pad:
+            x = self.pad(x)
+        x = self.conv(x)
+        if self.use_bn:
+            x = self.bn(x)
+        if self.activate:
+            x = self.activ(x)
+        return x
+
+def conv7x7_block(in_channels: int,
+                  out_channels: int,
+                  stride: int | tuple[int, int] = 1,
+                  padding: int | tuple[int, int] | tuple[int, int, int, int] = 3,
+                  dilation: int | tuple[int, int] = 1,
+                  groups: int = 1,
+                  bias: bool = False,
+                  use_bn: bool = True,
+                  bn_eps: float = 1e-5,
+                  activation = None) -> nn.Module:
+    
+    return ConvBlock(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=7,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups,
+        bias=bias,
+        use_bn=use_bn,
+        bn_eps=bn_eps,
+        activation=activation)
 
 
 class MLP(nn.Module):
@@ -179,60 +272,6 @@ class CbamBlock(nn.Module):
         return x
 
 
-class CbamResUnit(nn.Module):
-    """
-    CBAM-ResNet unit.
-
-    Parameters
-    ----------
-    in_channels : int
-        Number of input channels.
-    out_channels : int
-        Number of output channels.
-    stride : int or tuple(int, int)
-        Strides of the convolution.
-    bottleneck : bool
-        Whether to use a bottleneck or simple block in units.
-    """
-    def __init__(self,
-                 in_channels: int,
-                 out_channels: int,
-                 stride: int | tuple[int, int],
-                 bottleneck: bool):
-        super(CbamResUnit, self).__init__()
-        self.resize_identity = (in_channels != out_channels) or (stride != 1)
-
-        if bottleneck:
-            self.body = ResBottleneck(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                stride=stride,
-                conv1_stride=False)
-        else:
-            self.body = ResBlock(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                stride=stride)
-        if self.resize_identity:
-            self.identity_conv = conv1x1_block(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                stride=stride,
-                activation=None)
-        self.cbam = CbamBlock(channels=out_channels)
-        self.activ = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        if self.resize_identity:
-            identity = self.identity_conv(x)
-        else:
-            identity = x
-        x = self.body(x)
-        x = self.cbam(x)
-        x = x + identity
-        x = self.activ(x)
-        return x
-
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -290,7 +329,6 @@ class Bottleneck(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
@@ -322,11 +360,11 @@ class ResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0, ceil_mode=True)
-
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.dropout = nn.Dropout(p=0.5)
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -366,13 +404,15 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
+        
         x = self.avgpool(x)
+        #x = self.dropout(x)
         
         if not self.include_top:
             return x
         
         x = x.view(x.size(0), -1)
+        #x = self.dropout(x)
         x = self.fc(x)
         return x
 
@@ -434,20 +474,6 @@ def resnet50(pretrained=True, progress=True, out_classes = 7, use_cbam = False, 
         "resnet50", Bottleneck, [3, 4, 6, 3], pretrained, progress,num_classes = 1000, use_cbam = use_cbam, **kwargs
     )
     model.fc = nn.Linear(2048, out_classes)
-    return model
-
-def resnet50_vggface2(pretrained=True, progress=True,out_classes = 7,  use_cbam = False,  **kwargs):
-    r"""ResNet-50 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    model = _resnet(
-        "vggface2", Bottleneck, [3, 4, 6, 3], pretrained, progress,num_classes=8631, use_cbam = use_cbam, **kwargs
-    )
-    model.fc = nn.Linear(2048, out_classes)
-    print('model resnet50 with vggface2(trained from cratch) is done!')
     return model
 
 def resnet50_vggface2_ft(pretrained=True, progress=True,out_classes = 7,  use_cbam = False, **kwargs):
