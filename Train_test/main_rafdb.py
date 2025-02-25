9a632bfb1b0fc52bb76b8c44ef37381dc1227360
Azure2212 <dasanbob22122002@gmail.com>
2025-02-25 21:24:01 +0700

    update

--- Train_test/main_rafdb.py
+++ Train_test/main_rafdb.py
@@ -22,7 +22,7 @@ np.random.seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Datasets')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '/Utils/Datasets')))
 from rafdb_ds import RafDataSet
 
 from .Models.resnet import resnet50, resnet50_vggface2_ft
