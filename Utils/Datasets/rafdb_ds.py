aea430796ea373347634131e1b045d8076d01888
Azure2212 <dasanbob22122002@gmail.com>
2025-02-27 14:42:20 +0700

    update

--- Utils/Datasets/rafdb_ds.py
+++ Utils/Datasets/rafdb_ds.py
@@ -50,7 +50,7 @@ class RafDataSet(Dataset):
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Imagenet
-            transforms.Normalize(mean=[0.109, 0.109 0.109], std=[0.229, 0.224, 0.225]), # Imagenet
+            transforms.Normalize(mean=[0.109, 0.109, 0.109], std=[0.229, 0.224, 0.225]), # Imagenet
             #transforms.Normalize(mean=[0.5071, 0.4865, 0.4409], std=[0.2675, 0.2565, 0.2761]), # VGGface2
 
         ]
