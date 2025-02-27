d6a47b76f2d144bb0f3ba8a088454a0c05585ea1
Azure2212 <dasanbob22122002@gmail.com>
2025-02-27 19:39:06 +0700

    update

--- Utils/Datasets/fer2013_ds.py
+++ Utils/Datasets/fer2013_ds.py
@@ -33,6 +33,7 @@ class FER2013DataSet(Dataset):
             self.file_paths.extend(images)
             self.label.extend([emotion_mapping_rafdb_fer2013[emotion]]*len(images))
         print(self.file_paths)
+        print(self.label)
         self.transform = transforms.Compose(
         [
             transforms.ToPILImage(),
